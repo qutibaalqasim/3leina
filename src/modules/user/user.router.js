b87127ab766e2ddaee0b006d6de06b3859c8c064
qutibaalqasim <qalqasim9@gmail.com>
2025-04-27 19:28:09 +0300

    finsh get ActiveUsers end point

--- src/modules/user/user.router.js
+++ src/modules/user/user.router.js
@@ -8,6 +8,7 @@ import { auth } from "../../middleware/auth.js";
 const router = Router();
 
 router.get('/' ,auth(['super_Admin']), asyncHandler(controller.getAllUsers));
+router.get('/active' ,auth(['super_Admin']), asyncHandler(controller.getActiveUsers));
 
 
 
