76ce9c48bf2a8105ed1cf85cbc834c279bc91d65
qutibaalqasim <qalqasim9@gmail.com>
2025-04-27 12:16:37 +0300

    create userModel

---
+++ DB/models/user.model.js
@@ -0,0 +1,67 @@
+import mongoose, { model, Schema } from "mongoose";
+
+
+const userSchema = new Schema({
+    userName:{
+        type: String,
+        required: true,
+        unique: true,
+        min: 3,
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    password:{
+        type: String,
+        required: true,
+        min: 6,
+    },
+    phone:{
+        type: String,
+        unique: true,
+        min: 10,
+    },
+    address:{
+        type: String,
+        min: 5,
+    },
+    userImage:{
+        type: Object
+    },
+    confirmEmail:{
+        type: Boolean,
+        default: false,
+    },
+    gender:{
+        type: String,
+        enum:['Male' , 'Female']
+    },
+    status:{
+        type: String,
+        enum:['active' , 'inactive'],
+        default: 'active'
+    },
+    role:{
+        type: String,
+        enum:['super_Admin','admin', 'delivery_Agent' , 'user'],
+        default: 'user'
+    },
+    rating: {
+        type: Number,
+        default: 0,
+        min: 0,
+        max: 5,
+      },
+    sendCode:{
+        type: String,
+        default: null,
+    }
+},{
+    timestamps:true
+});
+
+const userModel = mongoose.model.User || model('User', userSchema);
+export default userModel;
