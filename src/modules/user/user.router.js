b4fa5ae8e86b85cddd0eaf869204f77941837a15
qutibaalqasim <qalqasim9@gmail.com>
2025-04-28 17:54:16 +0300

    modifying user.router.js

--- src/modules/user/user.router.js
+++ src/modules/user/user.router.js
@@ -8,16 +8,22 @@ import fileUpload, { fileValidation } from "../../utils/multer.js";
 
 
 const router = Router();
-
+// url/users
 router.get('/' ,auth(['super_Admin']), asyncHandler(controller.getAllUsers));
+// url/users/active
 router.get('/active' ,auth(['super_Admin']), asyncHandler(controller.getActiveUsers));
+// url/users/:id
 router.get('/:id' ,auth(['super_Admin']),validation(schema.getUserDetailsSchema), asyncHandler(controller.getUserDetails));
+// url/users/status/:id
 router.put('/status/:id',auth(['super_Admin']),validation(schema.changeUserStatusSchema), asyncHandler(controller.changeUserStatus));
+// url/users/:id
 router.put('/:id',auth(['super_Admin','admin', 'delivery_Agent' , 'user']),validation(schema.updateUserSchema), asyncHandler(controller.updateUser));
+// url/users/image/:id
 router.put('/image/:id',auth(['super_Admin','admin', 'delivery_Agent' , 'user']),fileUpload(fileValidation.image).fields([
     {name:"userImage" , maxCount:1}
 ])
 ,validation(schema.updateUserImageSchema), asyncHandler(controller.updateUserImage));
+// url/users/:id
 router.delete('/:id',auth(['super_Admin']),validation(schema.deleteUserSchema), asyncHandler(controller.deleteUser));
 
 export default router;
