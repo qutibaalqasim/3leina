d7c2713006c016d0201d447212148eb702a23467
qutibaalqasim <qalqasim9@gmail.com>
2025-04-28 19:22:50 +0300

    create category model

---
+++ DB/models/category.model.js
@@ -0,0 +1,39 @@
+import { Schema, Types } from "mongoose";
+
+
+const categorySchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+        min: 3,
+    },
+    slug: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        min: 5,
+    },
+    image: {
+        type: Object,
+    },
+    status: {
+        type: String,
+        enum: ['active', 'inactive'],
+        default: 'active'
+    },
+    createdBy:{
+        type: Types.ObjectId,
+        ref: 'User',
+    },
+    updatedBy:{
+        type: Types.ObjectId,
+        ref: 'User',
+    },
+}, { timestamps: true });
+
+
+const categoryModel = mongoose.model.Category || model('Category', categorySchema);
+export default categoryModel;
--- src/modules/deliveryAgent/delivery.controller.js
+++ src/modules/deliveryAgent/delivery.controller.js
@@ -8,7 +8,7 @@ export const createDelivery = async(req,res,next)=>{
     const {role} = req.body;
     const user = await userModel.findByIdAndUpdate(id, {role}, {new: true});
     if (!user) {
-        return next(new Error("User not found", { cause: 404 }));
+        return next(new AppError("User not found", { cause: 404 }));
     }
     return res.status(200).json({message:"success", user});
 }
@@ -16,7 +16,7 @@ export const createDelivery = async(req,res,next)=>{
 export const getAllDelivery = async (req, res,next) => {
     const delivery = await userModel.find({role: "delivery_Agent"});
     if (!delivery) {
-        return next(new Error("No delivery agents found", { cause: 404 }));
+        return next(new AppError("No delivery agents found", { cause: 404 }));
     }
     return res.status(200).json({message:"success", delivery});
 }
@@ -24,7 +24,7 @@ export const getAllDelivery = async (req, res,next) => {
 export const getActive = async (req,res,next)=>{
     const delivery = await userModel.find({role: "delivery_Agent", status: "active"});
     if (!delivery) {
-        return next(new Error("No active delivery agents found", { cause: 404 }));
+        return next(new AppError("No active delivery agents found", { cause: 404 }));
     }
     return res.status(200).json({message:"success", delivery});
 }
@@ -33,7 +33,7 @@ export const getDeliveryDetails = async (req,res,next)=>{
     const {id} = req.params;
     const delivery = await userModel.findById(id);
     if (!delivery) {
-        return next(new Error("Delivery agent not found",  404 ));
+        return next(new AppError("Delivery agent not found",  404 ));
     }
     return res.status(200).json({message:"success", delivery});
 }
