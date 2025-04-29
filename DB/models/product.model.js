c84021e992240ac1a987e31fcbd64010d4d1d99a
qutibaalqasim <qalqasim9@gmail.com>
2025-04-29 18:54:12 +0300

    create productModel

---
+++ DB/models/product.model.js
@@ -0,0 +1,65 @@
+import mongoose, { Schema, Types, model } from "mongoose";
+
+
+const productSchema = new Schema({
+    name:{
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+        min: 3,
+        max: 60,
+    },
+    slug:{
+        type: String,
+        required: true,
+    },
+    description:{
+        type: String,
+        required: true, 
+        trim: true,
+        min: 30,
+    },
+    stock:{
+        type: Number,
+        default: 1,
+    },
+    price:{
+        type: Number,
+        required: true,
+    },
+    discount:{
+        type: Number,
+        default: 0,
+    },
+    mainImage:{
+        type: Object,
+        required: true,
+    },
+    subImages:[
+        {
+            type: Object,
+        }
+    ],
+    status: {
+        type: String,
+        enum: ['active', 'inactive'],
+        default: 'active'
+    },
+    createdBy:{
+        type: Types.ObjectId,
+        ref: 'User',
+    },
+    updatedBy:{
+        type: Types.ObjectId,
+        ref: 'User',
+    },
+    subCategoryId:{
+        type: Types.ObjectId,
+        ref: 'SubCategory',
+        required: true,
+    }
+});
+
+const productModel = mongoose.model.Product || mongoose.model('Product', productSchema);
+export default productModel;
