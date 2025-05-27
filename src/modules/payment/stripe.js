7a9301ab4691f677d1b0334f9dae7ea276c49f13
qutibaalqasim <qalqasim9@gmail.com>
2025-05-27 20:04:59 +0300

    modify the stripe.js folder

--- src/modules/payment/stripe.js
+++ src/modules/payment/stripe.js
@@ -1,4 +1,11 @@
 import Stripe from "stripe";
 
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+    apiVersion: '2022-11-15',
+    typescript: true,
+    appInfo: {
+        name: '3leina',
+        version: '1.0.0',
+    },
+});
